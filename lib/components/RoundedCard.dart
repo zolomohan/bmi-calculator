55935f1a0cccaa7e661239d1c083e8decad82262
zolomohan <mohanzolomon@gmail.com>
2020-03-15 21:34:36 +0530

    Update Rounded Card to Accept Child

--- lib/components/RoundedCard.dart
+++ lib/components/RoundedCard.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class RoundedCard extends StatelessWidget {
 
   final Color color;
-
-  RoundedCard({this.color}){}
+  final Widget child;
+  RoundedCard({@required this.color, this.child}){}
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: child,
       margin: EdgeInsets.all(10.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20.0),
