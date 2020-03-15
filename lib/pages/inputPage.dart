71573ddb503a220276860b5977f984b296f38a1d
zolomohan <mohanzolomon@gmail.com>
2020-03-15 21:31:47 +0530

    Add Calculate Button

--- lib/pages/inputPage.dart
+++ lib/pages/inputPage.dart
@@ -1,6 +1,9 @@
 import 'package:bmi_calculator/components/RoundedCard.dart';
 import 'package:flutter/material.dart';
 
+const cardColor = Color(0xFF1D1E33);
+const calculateButtonColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -14,27 +17,40 @@ class _InputPageState extends State<InputPage> {
         title: Center(child: Text('BMI CALCULATOR')),
       ),
       body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(child: RoundedCard(color: Color(0xFF1D1E33))),
-                Expanded(child: RoundedCard(color: Color(0xFF1D1E33))),
-              ],
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(child: RoundedCard(color: cardColor)),
+                  Expanded(child: RoundedCard(color: cardColor)),
+                ],
+              ),
             ),
-          ),
-          Expanded(child: RoundedCard(color: Color(0xFF1D1E33))),
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(child: RoundedCard(color: Color(0xFF1D1E33))),
-                Expanded(child: RoundedCard(color: Color(0xFF1D1E33))),
-              ],
+            Expanded(child: RoundedCard(color: cardColor)),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(child: RoundedCard(color: cardColor)),
+                  Expanded(child: RoundedCard(color: cardColor)),
+                ],
+              ),
             ),
-          ),
-        ],
-      )),
+            Container(
+              color: calculateButtonColor,
+              width: double.infinity,
+              height: 60.0,
+              margin: EdgeInsets.only(top: 10.0),
+              child: Center(
+                child: Text(
+                  "Calculate Your BMI",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
