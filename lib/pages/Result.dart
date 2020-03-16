0c2dc2fdac930121ff2babc27295a14600796895
zolomohan <mohanzolomon@gmail.com>
2020-03-16 11:15:15 +0530

    Remove Appbar from Result Page

--- lib/pages/Result.dart
+++ lib/pages/Result.dart
@@ -11,7 +11,6 @@ class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('BMI CALCULATOR')),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
